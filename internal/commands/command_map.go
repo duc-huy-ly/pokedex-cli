febe6d43dce2777ea4259cc18050e7981ff5d40b
Duc Huy LY <lyduchuy@proton.me>
2026-05-17 23:10:11 +0200

    Implement caching mechanism in CommandMap and CommandMapB; add cache tests and refactor location handling

--- internal/commands/command_map.go
+++ internal/commands/command_map.go
@@ -7,9 +7,25 @@ import (
 )
 
 func CommandMap(c *pokeapi.Config) error {
-	if c.Next == "" {
+	url := c.Next
+	if url == "" {
 		return fmt.Errorf("No link to make the GET request.\n")
 	}
+	// Cache first
+	
+	data, exists := pokeapi.MyCache.Get(url)
+	if exists {
+		locations, err :=pokeapi.Convert(data)
+		if err != nil {
+			return err
+		}
+		for _, location := range locations.Results {
+			fmt.Println(location.Name)
+		}
+		return nil
+	}
+
+	// case not in case, do the api call
 	request, err := pokeapi.ListLocations(c.Next)
 	if err != nil {
 		return fmt.Errorf("%v\n", err)
